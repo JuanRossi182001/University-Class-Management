d048e129b8188de3bde413d535a8635611e57e80
Luis Busso <lbusso@email.unsl.edu.ar>
2024-05-23 20:44:11 -0300

    revert ruff formater

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
-from router import carrerRouter,classroomRouter,hourRouter,subjectRouter
-
+from router import carrerRouter, classroomRouter, hourRouter, subjectRouter
 
 
 app = FastAPI()
-app.include_router(carrerRouter.router)
-app.include_router(classroomRouter.router)
-app.include_router(hourRouter.router)
-app.include_router(subjectRouter.router)
-
+app.include_router(carrerRouter.router, prefix="/carrer", tags=["carer"])
+app.include_router(classroomRouter.router, prefix="/classroom", tags=["classroom"])
+app.include_router(
+    hourRouter.router,
+)
+app.include_router(subjectRouter.router, tags=["subjects"])
 
 
 @app.get("/")
--- router/carrerRouter.py
+++ router/carrerRouter.py
@@ -1,69 +1,60 @@
-from fastapi import APIRouter, HTTPException,Depends
-from schema.carrerSchema import RequestCarrer,Response
-from service.carrerService import create_carrer,delete_carrer,update_carrer,get_carrer_by_id,get_carrers
-from config.config import sessionlocal
+from fastapi import APIRouter, HTTPException, Depends
+from schema.carrerSchema import RequestCarrer, Response
+from service.carrerService import CarrerService
+from fastapi import status
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
 from typing import Annotated
 
 router = APIRouter()
 
 
-def get_db():
-    try:
-        db = sessionlocal()
-        yield db
-    finally:
-        db.close()
-        
-db_dependency = Annotated[Session,Depends(get_db)]
+dependency = Annotated[CarrerService, Depends()]
 
 
 # get all carrers end point
-@router.get("/carrers")
-async def get_all_carrers(db:db_dependency):
-    try:
-        _carrers = get_carrers(db=db)
-        return Response(code="200", status="OK",message="Succes fetch all data", result=_carrers).dict(exclude_none=True)
-    except:
-        raise HTTPException(status_code=400,detail="Fail to get data")
-    
-    
+@router.get("/")
+async def get_all(carrer_service: dependency):
+    _carrers = carrer_service.get_carrers()
+    return _carrers
+
+
 # get carrer by id end point
-@router.get("/carrers/{carrer_id}")
-async def get_by_id(carrer_id: int, db: db_dependency):
+@router.get("/{carrer_id}")
+async def get(carrer_id: int, carrer_service: dependency):
     try:
-        _carrer = get_carrer_by_id(db=db,carrer_id=carrer_id)
-        return Response(code="200", status="OK",message="Succes fetch all data", result=_carrer).dict(exclude_none=True)
-    except:
-        raise HTTPException(status_code=400,detail="Fail to get data") 
-    
-    
+        _carrer = carrer_service.get_carrer_by_id(carrer_id=carrer_id)
+        return _carrer
+    except NoResultFound as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+
+
 # create carrer end point
-@router.post("/carrers/create")
-async def create(carr: RequestCarrer, db: db_dependency):
-    try:
-        create_carrer(db=db,carrer=carr.parameter)
-        return Response(code="200", status="OK",message="Carrer created Successfully", result=None).dict(exclude_none=True)
-    except:
-        raise HTTPException(status_code=500,detail="Fail to create data")
-    
+@router.post("/create")
+async def create(carr: RequestCarrer, carrer_service: dependency):
+    carrer = carrer_service.create_carrer(carrer=carr)
+    return carrer
+
+
 # delete carrer end point
-@router.delete("/carrers/delete/{carrer_id}")
-async def delete(carrer_id: int, db: db_dependency):
+@router.delete("/delete/{carrer_id}")
+async def delete(carrer_id: int, carrer_service: dependency):
     try:
-        delete_carrer(db=db,carrer_id=carrer_id)
-        return Response(code="200", status="OK",message="Success delete data", result={}).dict(exclude_none=True)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Failed to create data: {str(e)}")
-        
-    
+        carrer=carrer_service.delete_carrer(carrer_id=carrer_id)
+        return carrer
+    except NoResultFound as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
 
 # update carrer end point
-@router.patch("/carrers/update/{carrer_id}")
-async def update(carrer_id: int, carr: RequestCarrer, db: db_dependency):
+@router.patch("/update/{carrer_id}")
+async def update(carrer_id: int, carr: RequestCarrer, carrer_service: dependency):
     try:
-        _carrer = update_carrer(db=db,carrer_id=carrer_id,name=carr.parameter.name,
-                                duration_in_years=carr.parameter.duration_in_years)
-        return Response(code="200",status="OK",message ="Succes update data", result =_carrer).dict(exclude_none = True)
+        _carrer = carrer_service.update_carrer(
+            carrer_id=carrer_id,
+            name=carr.parameter.name,
+            duration_in_years=carr.parameter.duration_in_years,
+        )
+        return _carrer
     except:
-        raise HTTPException(status_code=500,detail="Fail to update data")
+        raise HTTPException(status_code=500, detail="Fail to update data")
